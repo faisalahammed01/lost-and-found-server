4c351377f4af63325c66d2a97df0335aa6b6f882
/Faisalahammed003 <faisalahammed5035@gmail.com>
2025-05-11 23:11:46 +0600

    updated

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173/",
+      "http://localhost:5173",
       "https://lost-found-9fc15.web.app",
       "https://lost-found-9fc15.firebaseapp.com",
     ],
